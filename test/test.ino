380813759489945c26610d0a134cb8f694059d5c
tadeliss <tads.ra@gmail.com>
2020-02-25 18:26:33 +0200

    test--->hx711

--- test/test.ino
+++ test/test.ino
@@ -1,86 +1,26 @@
-#define BLYNK_PRINT Serial
-#include <Wire.h>
-#include <Adafruit_SSD1306.h>
-#include <Adafruit_GFX.h>
-#include <WiFi.h>
-#include <WiFiClient.h>
-#include <BlynkSimpleEsp32.h>
-#include <SimpleTimer.h>
-#include <TimeLib.h>
-#include <WidgetRTC.h>
-//long TimeSec;
-int skait;
+#include "HX711.h"
 
-char auth[] = "hKeENDdhyYgTWMNzOvLqoNfpO2tLg5oR";
-//Wifi duomenys
-char ssid[] = "Tado apartamentai";
-char pass[] = "tadas111";
+// HX711 circuit wiring
+const int LOADCELL_DOUT_PIN = 17;
+const int LOADCELL_SCK_PIN = 16;
 
-SimpleTimer timer;
-WidgetRTC rtc;
+HX711 scale;
 
-int isFirstConnect = true;
-BLYNK_CONNECTED() {
-if (isFirstConnect) {
- Blynk.syncAll();
- isFirstConnect = false;
-}
-}
 void setup() {
-Serial.begin(9600);  
- Blynk.begin(auth, ssid, pass);
-  while(Blynk.connect() == false);
-
-rtc.begin();
-  
- timer.setInterval(5000L, clockvalue);
- timer.setInterval(1000L, sync) ;
-  
-}
-
-BLYNK_WRITE(V4){
- skait = param.asInt();
-}
-BLYNK_WRITE(V5){
- long TimeSec = param[0].asLong();
- int val = hour();
- int minut = minute();
- long laikSec = val*3600+minut*60;
-if(TimeSec==laikSec){
-  for(int x; x<=skait; x++){
-    variklis();
-  }
- }
-}
-
-void sync(){
-  Blynk.syncVirtual(V4)
-  Blynk.syncVirtual(V5);
+  Serial.begin(57600);
+  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
 }
 
 void loop() {
-  Blynk.run();
-  timer.run(); 
 
-}
-
-void clockvalue()
-{
- int gmthour = hour();
-  if (gmthour == 24){
-     gmthour = 0;
+  if (scale.is_ready()) {
+    long reading = scale.read();
+    Serial.print("HX711 reading: ");
+    Serial.println(reading);
+  } else {
+    Serial.println("HX711 not found.");
   }
-  String displayhour =   String(gmthour, DEC);
-  int hourdigits = displayhour.length();
-  if(hourdigits == 1){
-    displayhour = "0" + displayhour;
-  }
-  String displayminute = String(minute(), DEC);
-  int minutedigits = displayminute.length();  
-  if(minutedigits == 1){
-    displayminute = "0" + displayminute;
-  }  
-  String displaycurrenttime = displayhour + ":" + displayminute;
-  Blynk.virtualWrite(V2, displaycurrenttime);
 
+  delay(1000);
+  
 }
